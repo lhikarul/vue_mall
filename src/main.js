a648097020444ab634f39715c13619f4ad4a1a5f
lhiakrul <711hikaru@gmail.com>
2020-06-17 21:56:19 +0800

    add: 接口錯誤攔截

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import App from './App.vue'
+
+
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000;
+
+// 接口錯誤攔截
+axios.interceptors.response.use(function(response){
+
+  let res = response.data;
+  if (res.status === 0) {
+    return res.data;
+  }else if (res.status === 10) {
+    window.location.href = '/#/login';  
+  }else {
+    alert (res.msg);
+  }
+}) 
+
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
