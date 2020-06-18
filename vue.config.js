9b6b60ac3d906e704c013d2d8cdbb51d6ad6a8d8
lhiakrul <711hikaru@gmail.com>
2020-06-18 20:57:28 +0800

    add: 導航組件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
+const path    = require('path');
+
+function resolve(dir) {
+    return path.join(__dirname, dir);
+}
 module.exports = {
-    
+    chainWebpack: (config) => {
+        config.resolve.alias
+            .set('@', resolve('src'))
+            .set('@C', resolve('src/components'))
+            .set('@style', resolve('src/resources/scss'))
+    },
 }
